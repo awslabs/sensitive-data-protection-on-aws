752721dbf7603ebf29122b123ea0f153e689d421
junzhong <junzhong@amazon.com>
2023-06-06 14:18:20 +0800

    fix(others): Change Version

--- source/portal/src/pages/add-account/types/add_account_type.ts
+++ source/portal/src/pages/add-account/types/add_account_type.ts
@@ -4,10 +4,10 @@ export const TAB_LIST = {
 };
 
 export const ADMIN_TEMPLATE_URL =
-  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/0.10.0/cn/Admin.template.json';
+  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/1.0.0/cn/Admin.template.json';
 
 export const AGENT_TEMPLATE_URL =
-  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/0.10.0/cn/Agent.template.json';
+  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/1.0.0/cn/Agent.template.json';
 
 export const IT_TEMPLATE_URL =
-  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/0.10.0/cn/IT.template.json';
+  'https://aws-gcr-solutions.s3.cn-north-1.amazonaws.com.cn/aws-sensitive-data-protection/1.0.0/cn/IT.template.json';
