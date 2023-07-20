33b6279837f669269f30b3590343be314ca1aa78
cuihubin <530051970@qq.com>
2023-07-20 16:58:38 +0800

    update version UI and batch insert identity mapping

--- source/portal/src/pages/signed-in-page/LayoutHeader.tsx
+++ source/portal/src/pages/signed-in-page/LayoutHeader.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import TopNavigation from '@cloudscape-design/components/top-navigation';
 import './style.scss';
 import { AmplifyConfigType, AppSyncAuthType } from 'ts/types';
-import { AMPLIFY_CONFIG_JSON, buildDocLink, CURRENT_VERSION } from 'ts/common';
+import { AMPLIFY_CONFIG_JSON, buildDocLink } from 'ts/common';
 import { User } from 'oidc-client-ts';
 import { RouterEnum } from 'routers/routerEnum';
 import { useTranslation } from 'react-i18next';
@@ -28,8 +28,8 @@ const LayoutHeader: React.FC<LayoutHeaderProps> = ({
   const { t, i18n } = useTranslation();
   const [fullLogoutUrl, setFullLogoutUrl] = useState('');
   const [oidcStorageId, setOidcStorageId] = useState('');
-
-  const version = localStorage.getItem(CURRENT_VERSION)||'';
+  const originConfig = localStorage.getItem(AMPLIFY_CONFIG_JSON);
+  const configData: AmplifyConfigType = JSON.parse(originConfig || '{}');
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -109,7 +109,7 @@ const LayoutHeader: React.FC<LayoutHeaderProps> = ({
                 },
                 {
                   id: 'version',
-                  text: t('header.version')+" "+version || '',
+                  text: t('header.version')+" "+configData.version || '',
                   disabled: true,
                 },
               ],
