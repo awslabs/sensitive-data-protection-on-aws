c41d6aed124922fcec2d0af671ade2c63069e8b3
Mike Yan <yiyanz@amazon.com>
2023-09-18 18:04:59 +0800

    feat(be): sync catalogs

--- source/constructs/api/lambda/crawler_event.py
+++ source/constructs/api/lambda/crawler_event.py
@@ -1,21 +1,26 @@
-import os
-import boto3
 import logging
+import os
+
+import boto3
+
+from common.enum import DatabaseType
 
 sqs = boto3.client('sqs')
 caller_identity = boto3.client('sts').get_caller_identity()
 partition = caller_identity['Arn'].split(':')[1]
 admin_account_id = caller_identity.get('Account')
 url_suffix = '.cn' if partition == 'aws-cn' else ''
+crawler_prefixes = [t.value + '-' for t in DatabaseType]
 logger = logging.getLogger("api")
+
 
 def lambda_handler(event, context):
     crawler_name = event['detail']['crawlerName']
-    if crawler_name.endswith('-crawler') and (crawler_name.startswith('rds-') or crawler_name.startswith('s3-')):
+    if crawler_name.endswith('-crawler') and (any(crawler_name.startswith(prefix) for prefix in crawler_prefixes)):
         message = sqs.send_message(
             QueueUrl=f"https://sqs.{os.getenv('AWS_REGION')}.amazonaws.com{url_suffix}/{os.getenv('ADMIN_ACCOUNT')}/{os.getenv('QUEUE')}",
             MessageBody=str(event))
-        logger.info(message)
+        logger.debug(message)
     return {
         'statusCode': 200
     }
