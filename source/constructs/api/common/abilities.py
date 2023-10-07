a422f0edc575bf310f2fb988f701ad51730c5d37
cuihubin <530051970@qq.com>
2023-10-07 10:07:30 +0800

    update common abilities

--- source/constructs/api/common/abilities.py
+++ source/constructs/api/common/abilities.py
@@ -2,13 +2,12 @@ from common.enum import (Provider,
                          DatabaseType)
 
 def convert_database_type_provider(database_type: str) -> int:
-    match database_type:
-        case DatabaseType.JDBC_ALIYUN.value:
-            return Provider.ALI_CLOUD.value
-        case DatabaseType.JDBC_TENCENT.value:
-            return Provider.TENCENT_CLOUD.value
-        case _:
-            return Provider.AWS_CLOUD.value
+    if database_type == DatabaseType.JDBC_ALIYUN.value:
+        return Provider.ALI_CLOUD.value
+    elif database_type == DatabaseType.JDBC_TENCENT.value:
+        return Provider.TENCENT_CLOUD.value
+    else:
+        return Provider.AWS_CLOUD.value
 
 def convert_provider_id_str(provider: int) -> str:
     if provider == Provider.TENCENT_CLOUD.value:
