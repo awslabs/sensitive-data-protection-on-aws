a7544b5c6c0f7cf73ba16ae3ddcb23d5bfbb9470
cuihubin <530051970@qq.com>
2023-10-07 09:58:48 +0800

    upgrade convert_database_type_provider

--- source/constructs/api/common/abilities.py
+++ source/constructs/api/common/abilities.py
@@ -2,26 +2,13 @@ from common.enum import (Provider,
                          DatabaseType)
 
 def convert_database_type_provider(database_type: str) -> int:
-    if database_type == DatabaseType.RDS.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.S3.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.S3_UNSTRUCTURED.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.GLUE.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.DDB.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.EMR.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.JDBC.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.JDBC_AWS.value:
-        return Provider.AWS_CLOUD.value
-    if database_type == DatabaseType.JDBC_ALIYUN.value:
-        return Provider.ALI_CLOUD.value
-    if database_type == DatabaseType.JDBC_TENCENT.value:
-        return Provider.TENCENT_CLOUD.value
+    match database_type:
+        case DatabaseType.JDBC_ALIYUN.value:
+            return Provider.ALI_CLOUD.value
+        case DatabaseType.JDBC_TENCENT.value:
+            return Provider.TENCENT_CLOUD.value
+        case _:
+            return Provider.AWS_CLOUD.value
 
 def convert_provider_id_str(provider: int) -> str:
     if provider == Provider.TENCENT_CLOUD.value:
