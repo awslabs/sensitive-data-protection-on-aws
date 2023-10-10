59febcc18246ed5e8efb7c00cb851e715e77e060
Xiujuan Li <xiujuali@amazon.com>
2023-10-10 15:24:18 +0800

    fix(be): fix crawler and database resources name

--- source/constructs/api/lambda/crawler_event.py
+++ source/constructs/api/lambda/crawler_event.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import boto3
-from common.constant import const
 
 logger = logging.getLogger("api")
 logger.setLevel(logging.INFO)
@@ -10,13 +9,14 @@ sqs = boto3.client('sqs')
 caller_identity = boto3.client('sts').get_caller_identity()
 partition = caller_identity['Arn'].split(':')[1]
 url_suffix = '.cn' if partition == 'aws-cn' else ''
+SOLUTION_NAME = "SDPS"
 
 
 def lambda_handler(event, context):
     logger.info(event)
     if 'detail' in event and 'crawlerName' in event['detail']:
         crawler_name = event['detail']['crawlerName']
-        if crawler_name.startswith(const.SOLUTION_NAME + "-"):
+        if crawler_name.startswith(SOLUTION_NAME + "-"):
             message = sqs.send_message(
                 QueueUrl=f"https://sqs.{os.getenv('AWS_REGION')}.amazonaws.com{url_suffix}/{os.getenv('ADMIN_ACCOUNT')}/{os.getenv('QUEUE')}",
                 MessageBody=str(event))
