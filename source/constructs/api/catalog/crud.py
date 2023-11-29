61541bfc7db054e928898d038ace986a3607f670
Xiujuan Li <xiujuali@amazon.com>
2023-11-29 14:00:22 +0800

    fix(be): fix catalog size and obj count

--- source/constructs/api/catalog/crud.py
+++ source/constructs/api/catalog/crud.py
@@ -810,8 +810,8 @@ def get_s3_database_summary_with_attr(database_type, attribute: str, need_merge:
                                 func.sum(models.CatalogDatabaseLevelClassification.object_count).label("object_total"),
                                 func.sum(models.CatalogDatabaseLevelClassification.size_key).label("size_total"),
                                 func.sum(models.CatalogDatabaseLevelClassification.table_count).label("table_total"))
-                         .filter(models.CatalogDatabaseLevelClassification.database_type.in_(
-            [DatabaseType.S3.value, DatabaseType.S3_UNSTRUCTURED.value]))
+                         .filter(models.CatalogDatabaseLevelClassification.database_type.in_([DatabaseType.S3.value,
+                                                                                              DatabaseType.S3_UNSTRUCTURED.value]))
                          .group_by(getattr(models.CatalogDatabaseLevelClassification, attribute))
                          .all()
                          )
@@ -828,9 +828,11 @@ def get_s3_database_summary_with_attr(database_type, attribute: str, need_merge:
             updated_database_list.append({
                 attribute: privacy,
                 "database_total": database["database_total"],
-                "object_total": object_total,
+                # "object_total": object_total,
+                "object_total": database["object_total"],
                 "size_total": database["size_total"],
-                "table_total": table_total
+                # "table_total": table_total
+                "table_total": database["table_total"]
             })
         for entry in table_list:
             privacy = entry["privacy"]
